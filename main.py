7b9539eeb0e77a4b58a0670d60cd2234e15c2944
aypetrunin <a.y.petrunin@gmail.com>
2025-10-24 13:32:16 +0300

    add mcp server annitta

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import asyncio
 from src.server.server_sse_alisa import mcp as mcp_alisa
 from src.server.server_sse_sofia import mcp as mcp_sofia
 from src.server.server_sse_anisa import mcp as mcp_anisa
+from src.server.server_sse_annitta import mcp as mcp_annitta
 from src.server.server_sse_anastasia import mcp as mcp_anastasia
 
 
 MCP_PORT_ALISA = int(os.getenv("MCP_PORT_ALISA"))  # 4011
 MCP_PORT_SOFIA = int(os.getenv("MCP_PORT_SOFIA"))  # 4012
 MCP_PORT_ANISA = int(os.getenv("MCP_PORT_ANISA"))  # 4015
+MCP_PORT_ANNITTA = int(os.getenv("MCP_PORT_ANNITTA"))  # 4016
 MCP_PORT_ANASTASIA = int(os.getenv("MCP_PORT_ANASTASIA"))  # 4017
 
 
@@ -18,6 +20,7 @@ async def main():
         mcp_alisa.run_async(transport="sse", port=MCP_PORT_ALISA, host="0.0.0.0"),
         mcp_sofia.run_async(transport="sse", port=MCP_PORT_SOFIA, host="0.0.0.0"),
         mcp_anisa.run_async(transport="sse", port=MCP_PORT_ANISA, host="0.0.0.0"),
+        mcp_annitta.run_async(transport="sse", port=MCP_PORT_ANNITTA, host="0.0.0.0"),
         mcp_anastasia.run_async(transport="sse", port=MCP_PORT_ANASTASIA, host="0.0.0.0"),
     )
 
--- src/server/server_sse_annitta.py
+++ src/server/server_sse_annitta.py
@@ -5,13 +5,14 @@ from ..tools.faq import tool_faq
 from ..tools.services import tool_services
 from ..tools.product_id import tool_record_product_id
 from ..tools.record_time import tool_record_time
-from ..tools.product_search_anisa import tool_product_search
+from ..tools.product_search_annitta import tool_product_search
 from ..tools.available_time_for_master import tool_available_time_for_master
 
 
-MCP_PORT_ANISA = int(os.getenv("MCP_PORT_ANISA"))  # 4015
+MCP_PORT_ANNITTA = int(os.getenv("MCP_PORT_ANNITTA"))  # 4016
 
-mcp = FastMCP(name="Anisa")
+mcp = FastMCP(name="Annitta")
+
 
 mcp.mount(tool_faq, 'zena')
 mcp.mount(tool_services, 'zena')
@@ -20,10 +21,9 @@ mcp.mount(tool_product_search, 'zena')
 mcp.mount(tool_record_product_id, 'zena')
 mcp.mount(tool_available_time_for_master, 'zena')
 
-
 if __name__ == "__main__":
-    mcp.run(transport="sse", port=MCP_PORT_ANISA, host="0.0.0.0")
+    mcp.run(transport="sse", port=MCP_PORT_ANNITTA, host="0.0.0.0")
 
 # cd /home/copilot_superuser/petrunin/mcp
-# nohup uv run python -m zena_qdrant.server_sse_anisa &
-# kill 1500178
+# nohup uv run python -m zena_qdrant.server_sse_annitta &
+# kill 1504239

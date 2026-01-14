9119bc00b1451727ca65ab6e116ca8a465419131
f_salih <f_salih@mail.ru>
2026-01-14 15:17:54 +0300

    update valentina

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from src.server.server_sse_anisa import mcp_anisa
 from src.server.server_sse_alena import mcp_alena
 from src.server.server_sse_annitta import mcp_annitta
 from src.server.server_sse_anastasia import  mcp_anastasia
+from src.server.server_sse_valentina import  mcp_valentina
 
 
 MCP_PORT_ALISA = int(os.getenv("MCP_PORT_ALISA"))  # 5001
@@ -16,7 +17,7 @@ MCP_PORT_ANISA = int(os.getenv("MCP_PORT_ANISA"))  # 5005
 MCP_PORT_ANNITTA = int(os.getenv("MCP_PORT_ANNITTA"))  # 5006
 MCP_PORT_ANASTASIA = int(os.getenv("MCP_PORT_ANASTASIA"))  # 5007
 MCP_PORT_ALENA = int(os.getenv("MCP_PORT_ALENA"))  # 5020
-
+MCP_PORT_VALENTINA = int(os.getenv("MCP_PORT_VALENTINA"))  # 5020
 
 async def main():
     await asyncio.gather(
@@ -26,6 +27,7 @@ async def main():
         mcp_annitta.run_async(transport="sse", port=MCP_PORT_ANNITTA, host="0.0.0.0"),
         mcp_anastasia.run_async(transport="sse", port=MCP_PORT_ANASTASIA, host="0.0.0.0"),
         mcp_alena.run_async(transport="sse", port=MCP_PORT_ALENA, host="0.0.0.0"),
+        mcp_valentina.run_async(transport="sse", port=MCP_PORT_VALENTINA, host="0.0.0.0"),
     )
 
 
